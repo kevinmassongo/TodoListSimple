31c6f9619233ee864ea14bd9ca6f6ef870a4bbad
Kevin Massongo <kevinmassongo196@gmail.com>
2023-10-20 20:47:09 +0100

    comprendre la logique de ce qu'on veut faire

---
+++ script.js
@@ -0,0 +1,30 @@
+const inputNom = document.getElementById('inputNom');
+const inputPrenom = document.getElementById('inputPrenom');
+const validation = document.getElementById('idForm')
+
+let nom = ''
+let prenom = ''
+
+inputNom.addEventListener('input', nomfunction)
+function nomfunction(e){
+    nom = e.target.value;
+}
+
+inputPrenom.addEventListener('input', prenomfunction)
+function prenomfunction(e){
+    prenom = e.target.value;
+}
+
+validation.addEventListener('submit', validfunction)
+
+function validfunction(e){
+    e.preventDefault()
+    if(svg.checked){
+        document.querySelector('table tbody').innerHTML += `<tr>
+        <td>${nom}</td>
+        <td>${prenom}</td>
+        </tr>`
+        inputNom.value = ''
+        inputPrenom.value = ''
+    }
+}
