e6681569dcd18414f69b6acfe3adca08ab085dc1
Kevin Massongo <kevinmassongo196@gmail.com>
2023-10-20 21:07:42 +0100

    Ajout d'un nouveau fichier au nom de validation

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const inputNom = document.getElementById('inputNom');
-const inputPrenom = document.getElementById('inputPrenom');
-const validation = document.getElementById('idForm')
+// const inputNom = document.getElementById('inputNom');
+// const inputPrenom = document.getElementById('inputPrenom');
+// const validation = document.getElementById('idForm')
 
-let nom = ''
-let prenom = ''
+// let nom = ''
+// let prenom = ''
 
-inputNom.addEventListener('input', nomfunction)
-function nomfunction(e){
-    nom = e.target.value;
-}
+// inputNom.addEventListener('input', nomfunction)
+// function nomfunction(e){
+//     nom = e.target.value;
+// }
 
-inputPrenom.addEventListener('input', prenomfunction)
-function prenomfunction(e){
-    prenom = e.target.value;
-}
+// inputPrenom.addEventListener('input', prenomfunction)
+// function prenomfunction(e){
+//     prenom = e.target.value;
+// }
 
-validation.addEventListener('submit', validfunction)
+// validation.addEventListener('submit', validfunction)
 
-function validfunction(e){
-    e.preventDefault()
-    if(svg.checked){
-        document.querySelector('table tbody').innerHTML += `<tr>
-        <td>${nom}</td>
-        <td>${prenom}</td>
-        </tr>`
-        inputNom.value = ''
-        inputPrenom.value = ''
-    }
-}
+// function validfunction(e){
+//     e.preventDefault()
+//     if(svg.checked){
+//         document.querySelector('table tbody').innerHTML += `<tr>
+//         <td>${nom}</td>
+//         <td>${prenom}</td>
+//         </tr>`
+//         inputNom.value = ''
+//         inputPrenom.value = ''
+//     }
+// }
