e7b18f5e6184edce432848e60e8b1ca45819ef58
Kevin Massongo <kevinmassongo196@gmail.com>
2023-10-21 02:36:37 +0100

    suppression du fichier validation.html

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+
 const inputNom = document.getElementById('inputNom');
 const inputPrenom = document.getElementById('inputPrenom');
-const validation = document.getElementById('idForm')
+const inputEmail = document.getElementById('email');
+const inputAge = document.getElementById('age');
+const inputPoste = document.getElementById('poste');
+const inputPhone = document.getElementById('phone');
+const inputSelect = document.querySelector('select');
+const inputPays = document.getElementById('pays');
+const main = document.querySelector('main');
+const section = document.querySelector('section');
+const btn1 = document.getElementById('btn1');
+const btnSubmit = document.getElementById('envoyer');
+const validation = document.getElementById('idForm');
 
-let nom = ''
-let prenom = ''
+let nom = '';
+let prenom = '';
+let email = '';
+let age = '';
+let poste = '';
+let phone = '';
+let select = '';
+let pays = '';
+
+btn1.addEventListener('click', btn1Function);
+
+function btn1Function(){
+    section.style.display = 'block';
+    main.style.display = 'none';
+}
+
+
 
 inputNom.addEventListener('input', nomfunction)
 function nomfunction(e){
@@ -15,16 +41,34 @@ function prenomfunction(e){
     prenom = e.target.value;
 }
 
+inputAge.addEventListener('input', ageFunction)
+function ageFunction(e){
+    age = e.target.value
+}
+
+inputEmail.addEventListener('input', emailFunction)
+function emailFunction(e){
+    email = e.target.value
+}
+
 validation.addEventListener('submit', validfunction)
 
 function validfunction(e){
     e.preventDefault()
+    
     if(svg.checked){
-        document.querySelector('table tbody').innerHTML += `<tr>
-        <td>${nom}</td>
-        <td>${prenom}</td>
-        </tr>`
-        inputNom.value = ''
-        inputPrenom.value = ''
+            document.querySelector('table tbody').innerHTML += `<tr>
+            <td>${nom}</td>
+            <td>${prenom}</td>
+            <td>${email}</td>
+            <td>${age}</td>
+            </tr>`
+            inputNom.value = '';
+            inputPrenom.value = '';
+            inputEmail.value = '';
+            inputAge.value = '';
+            main.style.display = 'block';
+            section.style.display = 'none';
     }
 }
+
