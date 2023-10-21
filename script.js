48466b33819bca8e6076a954c65c885a9f698749
Kevin Massongo <kevinmassongo196@gmail.com>
2023-10-21 03:01:06 +0100

    add codes

--- script.js
+++ script.js
@@ -29,8 +29,6 @@ function btn1Function(){
     main.style.display = 'none';
 }
 
-
-
 inputNom.addEventListener('input', nomfunction)
 function nomfunction(e){
     nom = e.target.value;
@@ -51,22 +49,47 @@ function emailFunction(e){
     email = e.target.value
 }
 
-validation.addEventListener('submit', validfunction)
+inputPoste.addEventListener('input', posteFunction)
+function posteFunction(e){
+    poste = e.target.value
+}
 
+inputPhone.addEventListener('input', phoneFunction)
+function phoneFunction(e){
+    phone = e.target.value
+}
+
+inputSelect.addEventListener('input', selectFunction)
+function selectFunction(e){
+    select = e.target.value
+}
+
+inputPays.addEventListener('input', paysFunction)
+function paysFunction(e){
+    pays = e.target.value
+}
+
+validation.addEventListener('submit', validfunction)
 function validfunction(e){
     e.preventDefault()
-    
     if(svg.checked){
             document.querySelector('table tbody').innerHTML += `<tr>
             <td>${nom}</td>
             <td>${prenom}</td>
             <td>${email}</td>
             <td>${age}</td>
+            <td>${poste}</td>
+            <td>${phone}</td>
+            <td>${select}</td>
+            <td>${pays}</td>
             </tr>`
             inputNom.value = '';
             inputPrenom.value = '';
             inputEmail.value = '';
             inputAge.value = '';
+            inputPoste.value = '';
+            inputSelect.value = '';
+            inputPays.value = '';
             main.style.display = 'block';
             section.style.display = 'none';
     }
